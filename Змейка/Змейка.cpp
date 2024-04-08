8e7058a39487883da461ac5bdf781df7541d1e72
user <user@WIN-GJ8V9JRLA9D>
2024-04-08 19:51:44 +0300

    Добавьте файлы проекта.

---
+++ Змейка/Змейка.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main() {
+    double number;
+
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+
+    double result = std::pow(number, 2);
+
+    std::cout << "The result is: " << result << std::endl;
+
+    return 0;
+}
